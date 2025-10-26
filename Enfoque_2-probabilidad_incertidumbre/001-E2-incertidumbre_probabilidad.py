6af1566f0c5fc3105765c145ab9953bdd0d66344
Alejandro Aguirre D. <159966137+AAD23110162@users.noreply.github.com>
2025-10-26 17:08:16 +0000

    Actualizacion E2.1

--- Enfoque_2-probabilidad_incertidumbre/001-E2-incertidumbre_probabilidad.py
+++ Enfoque_2-probabilidad_incertidumbre/001-E2-incertidumbre_probabilidad.py
@@ -1,12 +1,1 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Random Forest - Plantilla
-"""
 
-def main():
-    print("RandomForest - stub: implementar random forest aqui")
-
-
-if __name__ == '__main__':
-    main()
